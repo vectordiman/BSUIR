721135a107512e7df8b3bd11d53a73ad170ecf27
Dmitry Shangin <47499873+vectordiman@users.noreply.github.com>
2021-02-21 02:00:20 +0300

    Add files via upload

---
+++ COSII/main.py
@@ -0,0 +1,41 @@
+import fourier
+import plot
+
+
+def main():
+    # Всегда степень 2-ки
+    number = 64
+    x_list = fourier.create_x(number)
+    y_list = fourier.create_y(x_list)
+    frequency_list = fourier.create_frequency(number)
+
+    y_list_fft = fourier.create_fft(y_list, 1)
+    y_list_fft_reverse = fourier.create_fft(y_list_fft, -1)
+
+    y_list_dft = fourier.create_dft(y_list, number, 1)
+    y_list_dft_reverse = fourier.create_dft(y_list_dft, number, -1)
+
+    amplitude_list_fft = fourier.create_amplitude(y_list_fft, number)
+    amplitude_list_dft = fourier.create_amplitude(y_list_dft, number)
+    phase_list_fft = fourier.create_phase_fft(y_list_fft, number)
+    phase_list_dft = fourier.create_phase(y_list_dft, number)
+
+    plot.create_plot(x_list, y_list, 'График функции')
+
+    plot.create_stem(frequency_list, phase_list_dft, 'График фазового спектра (ДПФ)')
+    plot.create_stem(frequency_list, amplitude_list_dft, 'График амплитудного спектра (ДПФ)')
+    plot.create_plot(x_list, y_list_dft_reverse, 'Обратный график (ДПФ)')
+
+    plot.create_stem(frequency_list, phase_list_fft, 'График фазового спектра (БПФ)')
+    plot.create_stem(frequency_list, amplitude_list_fft, 'График амплитудного спектра (БПФ)')
+    plot.create_plot(x_list, y_list_fft_reverse, 'Обратный график (БПФ)')
+
+    print('Количество умножений (ДПФ): ' + str(fourier.counter_dft))
+    print('Количество умножений (БПФ): ' + str(fourier.counter_fft))
+
+    print('Время формирования (ДПФ): ' + str(fourier.time_dft))
+    print('Время формирования (БПФ): ' + str(fourier.time_fft))
+
+
+if __name__ == '__main__':
+    main()
---
+++ COSII/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot
+import matplotlib
+
+
+def create_plot(x_list, y_list, title):
+    fig, (ax) = matplotlib.pyplot.subplots(1, 1)
+
+    ax.plot(x_list, y_list)
+    ax.set(title=title)
+    ax.grid()
+
+    matplotlib.pyplot.show()
+
+
+def create_stem(x_list, y_list, title):
+    fig, (ax) = matplotlib.pyplot.subplots(1, 1)
+
+    ax.stem(x_list, y_list, markerfmt=' ')
+    ax.set(title=title)
+    ax.grid()
+
+    matplotlib.pyplot.show()
