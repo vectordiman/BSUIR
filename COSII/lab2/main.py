5115ab30e138b5e6ea341c3f3f0c960647d6ce56
Dmitry Shangin <47499873+vectordiman@users.noreply.github.com>
2021-03-12 11:51:07 +0300

    Add files via upload

--- COSII/lab2/main.py
+++ COSII/lab2/main.py
@@ -17,11 +17,21 @@ def main():
     plot.create_plot(x_list, y_list, 'Функция y = sin(2x)')
     plot.create_plot(x_list, z_list, 'Функция z = cos(7x)')
 
-    plot.create_plot(x_list, correlation_list, 'Корреляции')
-    plot.create_plot(x_list, correlation_fft_list, 'Корреляции БПФ')
+    plot.create_plot(x_list, correlation_list, 'Корреляция')
+    plot.create_plot(x_list, correlation_fft_list, 'Корреляция БПФ')
 
     plot.create_plot(x_list, convolution_list, 'Свертка')
     plot.create_plot(x_list, convolution_fft_list, 'Свертка БПФ')
+
+    print('\nКоличество математических операций (Корреляция): ' + str(int(operations.counter_correlation)))
+    print('Количество математических операций (Корреляция БПФ): ' + str(int(operations.counter_correlation_fft)))
+    print('Время формирования (Корреляция): ' + str(operations.time_correlation))
+    print('Время формирования (Корреляция БПФ): ' + str(operations.time_correlation_fft))
+
+    print('\nКоличество математических операций (Свертка): ' + str(int(operations.counter_convolution)))
+    print('Количество математических операций (Свертка БПФ): ' + str(int(operations.counter_convolution_fft)))
+    print('Время формирования (Свертка): ' + str(operations.time_convolution))
+    print('Время формирования (Свертка БПФ): ' + str(operations.time_convolution_fft))
 
 
 if __name__ == '__main__':
--- COSII/lab2/operations.py
+++ COSII/lab2/operations.py
@@ -1,5 +1,16 @@
+import time
 import fourier
 import numpy
+
+counter_correlation = 0
+counter_correlation_fft = 0
+counter_convolution = 0
+counter_convolution_fft = 0
+
+time_correlation = 0.0
+time_correlation_fft = 0.0
+time_convolution = 0.0
+time_convolution_fft = 0.0
 
 
 def create_x(length):
@@ -32,51 +43,84 @@ def get_real(points):
 
 
 def correlation(x_list, y_list, length):
+    global time_correlation
+    global counter_correlation
+
+    time_correlation = time.time()
     result = []
 
     for m in range(length):
         amount = 0
         for h in range(length):
             amount += x_list[h] * y_list[m + h - length]
+            counter_correlation += 2
         amount /= length
         result.append(amount / length)
+        counter_correlation += 2
 
+    time_correlation = time.time() - time_correlation
     return result
 
 
 def correlation_fft(x_list, y_list, length):
+    global time_correlation_fft
+    global counter_correlation_fft
+
+    time_correlation_fft = time.time()
+    fourier.counter_fft = 0
+
     cx = fourier.create_fft(x_list, 1, 1)
     cy = fourier.create_fft(y_list, 1, 1)
     result = []
 
     for i in range(length):
         cx[i] = numpy.conjugate(cx[i])
+        counter_correlation_fft += 1
     for i in range(length):
         result.append(cx[i] * cy[i] / length)
+        counter_correlation_fft += 2
 
     result = fourier.create_fft(result, -1, 4)
+    counter_correlation_fft += fourier.counter_fft
+    time_correlation_fft = time.time() - time_correlation_fft
     return result
 
 
 def convolution(x_list, y_list, length):
+    global time_convolution
+    global counter_convolution
+
+    time_convolution = time.time()
     result = []
 
     for m in range(length):
         amount = 0
         for h in range(length):
             amount += x_list[h] * y_list[m - h]
+            counter_convolution += 2
         result.append(amount / length)
+        counter_convolution += 1
 
+    time_convolution = time.time() - time_convolution
     return result
 
 
 def convolution_fft(x_list, y_list, length):
+    global time_convolution_fft
+    global counter_convolution_fft
+
+    time_convolution_fft = time.time()
+    fourier.counter_fft = 0
+
     cx = fourier.create_fft(x_list, 1, 1)
     cy = fourier.create_fft(y_list, 1, 1)
     result = []
 
     for i in range(length):
         result.append((cx[i] * cy[i]) / length)
+        counter_convolution_fft += 2
 
     result = fourier.create_fft(result, -1, 2)
+    counter_convolution_fft += fourier.counter_fft
+    time_convolution_fft = time.time() - time_convolution_fft
     return result
